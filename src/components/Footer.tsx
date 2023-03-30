eb6dfbf3fc454956d7fe6852907777896ed8adfe
Mitra, Raunak <raunakmitra8@gmail.com>
2023-03-30 19:17:22 +0530

    Add css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Button } from '@mui/material'
 
 const Footer = () => {
     return (
-        <div style={{ marginTop: 1120 }}>
+        <div style={{ marginTop: 1350, paddingTop: 80 }}>
             <div style={{ textAlign: 'center', backgroundColor: 'rgb(230,241,252)', height: 80, padding: 30, fontSize: 16 }}>
                 <span>We’d love to hear what you think!</span><br />
                 <Button sx={{ marginTop: 2, color: 'black', backgroundColor: 'white', borderColor: 'black', borderRadius: 9999 }} variant="outlined">
